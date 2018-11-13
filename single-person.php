22d8d856524b699c767a672a6570a8fe35f04326
Deon Frank <Deon.Frank@ucf.edu>
2018-11-13 10:02:23 -0500

    Update single-person.php

--- single-person.php
+++ single-person.php
@@ -19,35 +19,66 @@
 					<div class="person-job-title text-center mb-4"><?php echo $job_title; ?></div>
 					<?php endif; ?>
 
-					<?php echo get_person_contact_btns_markup( $post ); ?>
-
 					<?php echo get_person_dept_markup( $post ); ?>
 					<?php echo get_person_office_markup( $post ); ?>
 					<?php echo get_person_email_markup( $post ); ?>
 					<?php echo get_person_phones_markup( $post ); ?>
 
 				</aside>
+				<?php if ( $group_short_name = get_field( 'group_short_name' ) ): ?>
+				<p><a class="btn btn-primary mt-3" href="/<?php echo $group_short_name; ?>">Research Group Members</a></p>
+				<?php endif; ?>
 
+				<?php if ( $group_website = get_field( 'group_website' ) ): ?>
+				<p><a class="btn btn-primary mt-3" href="<?php echo $group_website; ?>">Research Group Website</a></p>
+				<?php endif; ?>
 			</div>
 
 			<div class="col-md-8 col-lg-7 pl-md-5">
 
 				<section class="person-content">
-					<?php echo get_person_desc_heading( $post ); ?>
-
 					<?php
 					if ( $post->post_content ) {
-						the_content();
-					}
-					else {
-						echo '<p>No description available.</p>';
-					}
-					?>
+						the_content(); } ?>
 
 					<?php if ( $cv_url = get_field( 'person_cv' ) ): ?>
 					<p>
-						<a class="btn btn-primary mt-3" href="<?php echo $cv_url; ?>">Download CV</a>
+						<a class="btn btn-primary mt-3" href="<?php echo $cv_url; ?>">Download Resume</a>
 					</p>
+					<?php endif; ?>
+
+					<?php if ( have_rows( 'person_links', $post->ID ) ): ?>
+						<h5 class="person-subheading mt-5">Links</h5>
+						<div class="row">
+						<div class="">
+							<ul class="list-unstyled mb-0">
+							<?php while ( have_rows( 'person_links', $post->ID ) ): the_row();
+								$link_title = get_sub_field( 'title' );
+								$link_url = get_sub_field( 'url' ); ?>
+								<li>
+									<a href="<?php echo $link_url; ?>" class="person-link" target="_blank">
+										<?php echo $link_title; ?></a>
+								</li>
+							<?php endwhile; ?>
+							</ul>
+						</div>
+						</div>
+					<?php endif; ?>
+
+					<?php if ( have_rows( 'person_awards', $post->ID ) ): ?>
+						<h5 class="person-subheading mt-5">Awards</h5>
+						<div class="row">
+						<div class="">
+							<ul class="list-unstyled mb-0">
+							<?php while ( have_rows( 'person_awards', $post->ID ) ): the_row();
+								$award = get_sub_field( 'award' ); ?>
+								<li>
+									<?php echo $award; ?>
+								</li>
+							<?php endwhile; ?>
+							</ul>
+						</div>
+						</div>
 					<?php endif; ?>
 				</section>
 
