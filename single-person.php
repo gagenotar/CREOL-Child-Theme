8e27961100efd80213378b552b53bb64a4a587e7
Deon Frank <Deon.Frank@ucf.edu>
2019-02-14 12:42:38 -0500

    Update single-person.php

--- single-person.php
+++ single-person.php
@@ -2,9 +2,9 @@
 
 <article class="<?php echo $post->post_status; ?> post-list-item">
 	<div class="container my-5">
-					<?php
-					if ( $post->post_content ) {
-						the_content(); } ?>
+		<?php
+		if ( $post->post_content ) {
+		the_content(); } ?>
 	</div>
 </article>
 
