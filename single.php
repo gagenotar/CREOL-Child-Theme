3b38ee1e6162f0d76eff8353dc471cfeb5246ec6
Deon Frank <Deon.Frank@ucf.edu>
2019-03-14 09:58:06 -0400

    Blank post for Elementor

---
+++ single.php
@@ -0,0 +1,11 @@
+<?php get_header(); the_post(); ?>
+
+<article class="<?php echo $post->post_status; ?> post-list-item">
+	<div class="container my-5">
+		<?php
+		if ( $post->post_content ) {
+		the_content(); } ?>
+	</div>
+</article>
+
+<?php get_footer(); ?>
