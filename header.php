d69cf82dc9631d477870c120f9af3d96e6b7591c
Deon Frank <Deon.Frank@ucf.edu>
2019-10-29 15:58:01 -0400

    Removed titles for people and posts

--- header.php
+++ header.php
@@ -6,6 +6,25 @@
 	<body ontouchstart <?php body_class(); ?>>
 		<?php do_action( 'after_body_open' ); ?>
 		<header class="site-header">
-			<?php echo get_header_markup(); ?>
+			<?php 
+			//echo get_header_markup(); 
+				$videos = $images = null;
+				$obj = get_queried_object();
+
+				if ( is_single() || is_page() ) {
+					$videos = get_header_videos( $obj );
+					$images = get_header_images( $obj );
+				}
+
+				echo get_nav_markup();
+
+				if ( $videos || $images ) {
+					echo get_header_media_markup( $obj, $videos, $images );
+				}
+			// removed titles for people and posts
+			// end of get_header_markup
+			// Additional CSS
+			// body.elementor-page-4828 h1.entry-title, .elementor-page-title {display: block !important;}
+			?>
 		</header>
 		<main id="main" class="site-main">
